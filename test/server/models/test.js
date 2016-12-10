b54965d767f28704a9ad21cf96e410fb83193f71
Daniel <dsmit192@mail.depaul.edu>
2016-12-09 18:15:30 -0600

    final edits

--- test/server/models/test.js
+++
@@ -1,7 +0,0 @@
-var mongoose   = require('mongoose');
-
-//Schemas go here!
-var ExampleSchema = new mongoose.Schema({
-//what is in the schema?
-});
-var Example = mongoose.model('Example', ExampleSchema)
