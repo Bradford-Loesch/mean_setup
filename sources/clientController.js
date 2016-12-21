9a9c22b92baffd502aff3ca323a0c8fb64afbc47
Bradford Loesch <bradford.loesch@gmail.com>
2016-12-21 13:29:06 -0600

    updated client controller/factory interactions

--- sources/clientController.js
+++ sources/clientController.js
@@ -2,34 +2,33 @@ app.controller("SCHEMANAMEController", ["$scope", "SCHEMANAMEFactory", function 
     $scope.VARIABLENAMEs = [];
     $scope.VARIABLENAME = {};
 
-    $scope.getSCHEMANAMEs = function() {
+    $scope.setSCHEMANAMEs = function() {
         SCHEMANAMEFactory.index(function (data) {
             $scope.VARIABLENAMEs = data;
         });
     }
-    $scope.getSCHEMANAMEs();
+    $scope.setSCHEMANAMEs();
 
     function setSCHEMANAME(data) {
         $scope.VARIABLENAME = data;
+        $scope.newSCHEMANAME = {};
     }
+
     $scope.show = function() {
-        SCHEMANAMEFactory.show($routeParams.id, setSCHEMANAME);
+        SCHEMANAMEFactory.show(setSCHEMANAME);
     }
 
     $scope.create = function (newSCHEMANAME)
     {
-        SCHEMANAMEFactory.create(newSCHEMANAME, function (data) {
-            $scope.VARIABLENAMEs.push(data);
-            newSCHEMANAME = {};
-        });
+        SCHEMANAMEFactory.create(newSCHEMANAME, setSCHEMANAMEs);
     };
 
     $scope.update = function() {
-        SCHEMANAMEFactory.update($routeParams.id, $scope.newSCHEMANAME);
+        SCHEMANAMEFactory.update($scope.newSCHEMANAME, setSCHEMANAMEs);
         newSCHEMANAME = {};
     }
 
     $scope.delete = function(id) {
-        SCHEMANAMEFactory.delete(id, setSCHEMANAMEs);
+        SCHEMANAMEFactory.delete(setSCHEMANAMEs);
     }
 }]);
